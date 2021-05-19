074f8a97e88fdefb1abeb33ca0efc2e6ebfb746c
Karol Ćwiąkała <karol.cwiakala@gmail.com>
2021-05-19 13:07:55 +0200

    Zadanie 1.2

--- zadanie_4.py
+++ zadanie_4.py
@@ -1,15 +1,9 @@
 """Zadanie 1.2"""
 
-# Potem napisz taki program: użytkownik podaje swój wiek i ile nocy spędzi w hotelu, a program wylicza,
-# ile trzeba zapłacić. Zasady są takie:
-# - nieletni (poniżej 18 roku życia) płacą 100 zł za noc
-# - dorośli (ci, którzy mają przynajmniej 18 lat ale mniej niż 65 lat) płacą:
-# 	- 200 zł za noc, jeśli nocują jedną noc
-# 	- 180 zł za noc, jeśli nocują przynajmniej dwie ale mniej niż pięć nocy
-# 	- 150 zł za noc, jeśli nocują pięć lub więcej nocy
-# - emeryci (ci, którzy mają przynajmniej 65 lat), płacą jak dorośli, ale z 10% zniżki
-# Przykładowo: jeśli użytkownik ma 70 lat i spędzi w hotelu dziesięć nocy,
-# zapłaci 150 zł za noc z 10% zniżki, czyli 150-15 zł za noc, czyli 135 zł za noc, czyli 1350 zł.
+# Napisz taki program: użytkownik ma podać, w jaki dzień tygodnia oddał buty do szewca (poniedziałek to dzień 1,
+# wtorek to dzień 2 itp.). Ma też podać, ile dni będzie trwała naprawa.
+# Program ma wypisać, w jaki dzień tygodnia buty będą gotowe do odbioru.
+# Jeśli tak będzie ci wygodniej, możesz założyć, że naprawa butów nigdy nie będzie trwała dłużej niż siedem dni.
 
 dni_tygodnia = ['poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota', 'niedziela']
 a = input("Podaj dzień oddania butów: ").lower()
