b62fdc64717b5908369c74adc992068dbe1f06f2
Karol Ćwiąkała <karol.cwiakala@gmail.com>
2021-05-19 12:56:03 +0200

    Zadanie 1.1 - 3

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,5 +1,9 @@
 """Zadanie 1.1 - 3"""
 
+# Napisz program, który dla podanych liczb: wzrostu w cm i masy ciała w kg obliczą i wypisze współczynnik BMI,
+# oraz podsumowanie informujące o stanie/zaleceniach. (Informacje o BMI: wzór,
+# interpretację wyników, proszę znaleźć samodzielnie).
+
 cena_z = float(input("Podaj cenę kilograma ziemniaków: ").split()[0])
 ile_z = float(input("Ile kilogramów ziemniaków chcesz kupić?: ").split()[0])
 cena_b = float(input("Podaj cenę kilograma bananów: ").split()[0])
