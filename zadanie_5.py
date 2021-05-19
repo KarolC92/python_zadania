39aba41bc221b1fd2b62ff2013cc667f1274fbd0
Karol Ćwiąkała <karol.cwiakala@gmail.com>
2021-05-19 13:08:04 +0200

    Zadanie 1.3

--- zadanie_5.py
+++ zadanie_5.py
@@ -1,5 +1,9 @@
 """Zadanie 1.3"""
 
+# Napisz program, który dla podanych liczb: wzrostu w cm i masy ciała w kg obliczą i wypisze współczynnik BMI,
+# oraz podsumowanie informujące o stanie/zaleceniach. (Informacje o BMI: wzór,
+# interpretację wyników, proszę znaleźć samodzielnie).
+
 masa = float(input("Podaj masę swojego ciała w kg: "))
 wzrost = int(input("Podaj swój wzrost w cm: "))
 
