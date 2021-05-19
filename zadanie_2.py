b89585a8d3f47dbb59897bc8244e44432ef70757
Karol Ćwiąkała <karol.cwiakala@gmail.com>
2021-05-19 13:07:32 +0200

    Zadanie 1.1 - 2

--- zadanie_2.py
+++ zadanie_2.py
@@ -1,9 +1,8 @@
 """Zadanie 1.1 - 2"""
 
-# Napisz taki program: użytkownik ma podać, w jaki dzień tygodnia oddał buty do szewca (poniedziałek to dzień 1,
-# wtorek to dzień 2 itp.). Ma też podać, ile dni będzie trwała naprawa.
-# Program ma wypisać, w jaki dzień tygodnia buty będą gotowe do odbioru.
-# Jeśli tak będzie ci wygodniej, możesz założyć, że naprawa butów nigdy nie będzie trwała dłużej niż siedem dni.
+# Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał,
+# ile kosztuje kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli,
+# ile trzeba będzie zapłacić za te ziemniaki.
 
 cena = float(input("Podaj cenę jednego kilograma ziemniaków: ").split()[0])
 ile = float(input("Ile kilogramów ziemniaków chcesz kupić?: ").split()[0])
